4d46093b8c1003c0d36f644d550b7ca405e91986
Anushkacoder37 <bhardwajanushka88@gmail.com>
2025-12-17 11:04:11 +0530

    first commit

--- src/main/java/com/usermanagement/usermanagement/config/SecurityConfig.java
+++ src/main/java/com/usermanagement/usermanagement/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.usermanagement.usermanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,17 +17,20 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        // Swagger URLs
+                        // Swagger
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
-                        // APIs
+
+                        // APIs secured
                         .requestMatchers("/api/**").authenticated()
+
+                        // others
                         .anyRequest().permitAll()
                 )
-                .httpBasic(basic -> {})
+                .httpBasic(Customizer.withDefaults())
                 .formLogin(form -> form.disable());
 
         return http.build();
@@ -37,3 +41,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
+
