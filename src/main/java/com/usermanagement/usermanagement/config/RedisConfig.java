344c8fbda3979351ce4ba5d79e420e6d2a0f406d
Anushkacoder37 <bhardwajanushka88@gmail.com>
2025-12-18 10:16:26 +0530

    first commit

--- src/main/java/com/usermanagement/usermanagement/config/RedisConfig.java
+++ src/main/java/com/usermanagement/usermanagement/config/RedisConfig.java
@@ -3,10 +3,13 @@ package com.usermanagement.usermanagement.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -38,4 +41,16 @@ public class RedisConfig {
                         )
                 );
     }
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port,
+            @Value("${spring.redis.password}") String password) {
+        RedisStandaloneConfiguration config =
+                new RedisStandaloneConfiguration(host, port);
+        config.setPassword(password);
+
+        return new LettuceConnectionFactory(config);
+    }
+
 }
